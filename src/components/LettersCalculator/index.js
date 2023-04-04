4531d1335f85ecf40eee52ac6ea1d464b866ecc9
saishruthi <nagamallashruthi430.com>
2023-04-04 08:26:08 +0000

    change

--- src/components/LettersCalculator/index.js
+++ src/components/LettersCalculator/index.js
@@ -4,39 +4,48 @@ import {Component} from 'react'
 import './index.css'
 
 class LettersCalculator extends Component {
-  state = {count: 0}
+  state = {
+    inputPhrase: '',
+  }
 
-  onEnterText = event => {
-    this.setState({count: event.target.value})
+  onChangeInputPhrase = event => {
+    const {value} = event.target
+
+    this.setState({inputPhrase: value})
   }
 
   render() {
-    const {count} = this.state
+    const {inputPhrase} = this.state
 
     return (
-      <div className="container">
-        <div className="bg">
-          <div className="card">
-            <h1 className="head">Calculate the Letters you enter</h1>
-            <label htmlFor="phrase Text">Enter the phrase</label>
-            <input
-              type="text"
-              className="search"
-              id="phrase Text"
-              onChange={this.onEnterText}
-              value={count}
-              placeholder="Enter the phrase"
-            />
-            <p className="p">No.of letters: {count.length}</p>
+      <div className="app-container">
+        <div className="letters-calculator-container">
+          <div className="calculator-container">
+            <h1 className="heading">Calculate the Letters you enter</h1>
+            <div className="input-phrase-container">
+              <label className="label" htmlFor="phraseText">
+                Enter the phrase
+              </label>
+              <input
+                className="letters-input"
+                id="phraseText"
+                onChange={this.onChangeInputPhrase}
+                placeholder="Enter the phrase"
+                type="text"
+                value={inputPhrase}
+              />
+            </div>
+            <p className="letters-count">No.of letters: {inputPhrase.length}</p>
           </div>
           <img
-            src="https://assets.ccbp.in/frontend/react-js/stop-watch-with-calculator-img.png"
             alt="letters calculator"
-            className="img"
+            className="letters-calculator-image"
+            src="https://assets.ccbp.in/frontend/react-js/stop-watch-with-calculator-img.png"
           />
         </div>
       </div>
     )
   }
 }
+
 export default LettersCalculator
